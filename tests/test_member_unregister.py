aaf4603cf9db37c11e4868f5264a40fc61729f65
mahdinazari <mehdinazari@carrene.com>
2019-09-08 17:23:47 +0430

    Add unregister member test

---
+++ tests/test_member_unregister.py
@@ -0,0 +1,42 @@
+from bddrest.authoring import response, given, when, status
+
+from inception.models import Member
+from .helpers import LocalApplicationTestCase
+
+
+class TestMember(LocalApplicationTestCase):
+
+    @classmethod
+    def mockup(cls):
+        session = cls.create_session()
+        cls.member = Member(
+            email='already.addted@example.com',
+            name='user_name',
+            family='family',
+            password='123abc',
+
+        )
+        session.add(cls.member)
+        session.commit()
+
+    def test_member_unregister(self):
+        with self.given(
+            'Unregister a member',
+            f'/apiv1/members/id: {self.member.id}',
+            'UNREGISTER',
+        ):
+            assert status == 200
+            assert response.json['id'] == self.member.id
+
+            when(
+                'Member not found',
+                url_parameters=given | dict(id=0)
+            )
+            status == 404
+
+            when(
+                'Member not found',
+                url_parameters=given | dict(id='Alphabetical')
+            )
+            status == 404
+
